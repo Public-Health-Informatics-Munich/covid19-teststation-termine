ae1147113089a6f7e44546df32f30db7a5122d8c
Christian Albrecht <christian.albrecht@mayflower.de>
2020-04-09 17:56:35 +0200

    yarn run format

--- termine-fe/src/Components/CouponBox.js
+++ termine-fe/src/Components/CouponBox.js
@@ -1,16 +1,36 @@
 import React from "react";
 
-export const CouponBox = ({coupons}) => {
-    return <div id="coupons">
-        <h3 style={{paddingLeft: "calc(2 * var(--universal-padding))"}}>
-            {coupons > 0
-             ? <div>Sie können
-               noch <b><u>{coupons} Termine</u></b> vergeben.</div>
-             : (coupons === 0
-                ? <><div>Sie können <b><u>keine Termine</u></b> mehr vergeben.</div>
-                  <div dangerouslySetInnerHTML={{__html: window.config.contactInfoCoupons}}/></>
-                : <div>&nbsp;</div>)
-                }
-        </h3>
+export const CouponBox = ({ coupons }) => {
+  return (
+    <div id="coupons">
+      <h3 style={{ paddingLeft: "calc(2 * var(--universal-padding))" }}>
+        {coupons > 0 ? (
+          <div>
+            Sie können noch{" "}
+            <b>
+              <u>{coupons} Termine</u>
+            </b>{" "}
+            vergeben.
+          </div>
+        ) : coupons === 0 ? (
+          <>
+            <div>
+              Sie können{" "}
+              <b>
+                <u>keine Termine</u>
+              </b>{" "}
+              mehr vergeben.
+            </div>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: window.config.contactInfoCoupons,
+              }}
+            />
+          </>
+        ) : (
+          <div>&nbsp;</div>
+        )}
+      </h3>
     </div>
-}
+  );
+};
