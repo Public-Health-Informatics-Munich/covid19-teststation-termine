8936a862292ab593ee19f1212704bb2050d2bd29
Valentin Schaeidt <valentin.schaeidt@mayflower.de>
2021-02-02 16:59:44 +0100

    adapt tests to new JWT auth in user facing frontend

--- termine-be/admin_api/admin_api_test.py
+++ termine-be/admin_api/admin_api_test.py
@@ -2,7 +2,7 @@ import hug
 
 import main
 from conftest import get_user_login, get_admin_login, get_create_user_pw_mismatch, USER, get_create_user, \
-    get_auth_header, get_invalid_login
+    get_admin_auth_header, get_valid_user_auth_header, get_invalid_login
 
 
 def test_user_is_unauthorized(testing_db):
@@ -35,12 +35,12 @@ def test_create_user_already_exists(testing_db):
 def ignore_test_create_user(testing_db):
     username = "test"
     password = "test123"
-    response = hug.test.get(main, "/config.js", headers=get_auth_header(username, password))
+    response = hug.test.get(main, "/config.js", headers=get_valid_user_auth_header(username, password))
     assert response.status == hug.HTTP_401
     response = hug.test.put(main, "/admin_api/user", headers=get_admin_login(),
                             body=get_create_user(username, password))
     assert response.status == hug.HTTP_200
-    response = hug.test.get(main, "/config.js", headers=get_auth_header(username, password))
+    response = hug.test.get(main, "/config.js", headers=get_valid_user_auth_header(username, password))
     assert response.status == hug.HTTP_200
-    response = hug.test.get(main, "/admin_api/user", headers=get_auth_header(username, password))
+    response = hug.test.get(main, "/admin_api/user", headers=get_admin_auth_header(username, password))
     assert response.status == hug.HTTP_401
--- termine-be/cli_test.py
+++ termine-be/cli_test.py
@@ -7,7 +7,7 @@ import hug
 import pytest
 
 import main
-from conftest import ADMIN, USER, get_user_login, get_basic_auth, get_admin_login
+from conftest import ADMIN, USER, get_user_login, get_basic_auth, get_admin_login, get_user_login_body
 from db.model import User, TimeSlot, Appointment, Booking
 
 
@@ -263,8 +263,8 @@ def test_delete_timeslots(testing_db):
 
 def test_change_user_pw(testing_db):
     NEW_PASSWORD = 'NEW_PASSWORD'
-    # first, verify that both users' logins work
-    response = hug.test.get(main, "/api/booked", headers=get_user_login(), start_date="2020-03-26", end_date="2020-03-26")
+    # first, verify that both user and admin logins work
+    response = hug.test.post(main, "/login", body=get_user_login_body())
     assert response.status == hug.HTTP_200
     response = hug.test.get(main, "/admin/config.js", headers=get_admin_login())
     assert response.status == hug.HTTP_200
@@ -272,11 +272,11 @@ def test_change_user_pw(testing_db):
     hug.test.cli('change_user_pw', module='main', username=USER, password=NEW_PASSWORD, for_real=True)
 
     # check that the old login does not work anymore
-    response = hug.test.get(main, "/api/booked", headers=get_user_login(), start_date="2020-03-26", end_date="2020-03-26")
-    assert response.status == hug.HTTP_401
+    response = hug.test.post(main, "/login", body=get_user_login_body())
+    assert response.status == hug.HTTP_400
 
     # and the new one does
-    response = hug.test.get(main, "/api/booked", headers={"Authorization": get_basic_auth(USER, NEW_PASSWORD)}, start_date="2020-03-26", end_date="2020-03-26")
+    response = hug.test.post(main, "/login", body=get_user_login_body(password=NEW_PASSWORD))
     assert response.status == hug.HTTP_200
 
     # and the existing user wasn't changed
